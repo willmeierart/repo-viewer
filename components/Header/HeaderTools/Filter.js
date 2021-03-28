b2a9a1f8e9c3ea7f3db9d6bfdc0797e2e8ab5125
WILL MEIER <willmeierart@gmail.com>
2021-03-28 11:55:28 -0600

    Add filters

---
+++ components/Header/HeaderTools/Filter.js
@@ -0,0 +1,115 @@
+// PACKAGES
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import clsx from "clsx";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import Tooltip from "@material-ui/core/Tooltip";
+import { makeStyles } from "@material-ui/core/styles";
+// REDUX
+import { filterList } from "../../../redux/actions";
+// CONSTANTS
+import { FILTERS } from "../../../lib/constants";
+
+const useToolbarStyles = makeStyles((theme) => ({
+  drawer: {
+    width: 250,
+  },
+  root: {
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(1),
+  },
+  title: {
+    flex: "1 1 100%",
+  },
+}));
+
+/**
+ * @Component
+ * The drawer that contains filter options
+ * Only accessible via the homepage
+ *
+ */
+const Filter = () => {
+  const classes = useToolbarStyles();
+
+  const dispatch = useDispatch();
+  const {
+    data: { filters, order, orderBy, searchPhrase },
+  } = useSelector((state) => state);
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const handleFilter = (filterKey) => ({ target: { value } }) => {
+    dispatch(
+      filterList(
+        { [filterKey]: value },
+        { filters, order, orderBy, searchPhrase }
+      )
+    );
+  };
+
+  const toggleDrawer = (open) => (e) => {
+    setDrawerOpen(open);
+  };
+
+  return (
+    <>
+      <Tooltip title="Filter transactions">
+        <IconButton aria-label="filter toggle" onClick={toggleDrawer(true)}>
+          <FilterListIcon />
+        </IconButton>
+      </Tooltip>
+      <Drawer anchor="right" onClose={toggleDrawer(false)} open={drawerOpen}>
+        <div className={clsx(classes.drawer)} role="presentation">
+          <List>
+            <ListItem>
+              <ListItemText>Filter</ListItemText>
+            </ListItem>
+            <Divider />
+            {FILTERS.map(({ display, id, options }) => {
+              const val = options.find((option) => option === filters[id]);
+              return (
+                <ListItem key={id}>
+                  <Select
+                    aria-label={`filter dropdown ${id}`}
+                    displayEmpty
+                    onChange={handleFilter(id)}
+                    placeholder={display}
+                    renderValue={(rVal) => rVal || display}
+                    value={val || display}
+                  >
+                    <MenuItem key="delete-option" value={display}>
+                      <IconButton>
+                        <DeleteIcon />
+                      </IconButton>
+                    </MenuItem>
+                    {options.map((option) => (
+                      <MenuItem
+                        aria-label={`filter ${id} option ${option}`}
+                        key={`${id}-${option}`}
+                        value={option}
+                      >
+                        {option}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </ListItem>
+              );
+            })}
+          </List>
+        </div>
+      </Drawer>
+    </>
+  );
+};
+
+export default Filter;
